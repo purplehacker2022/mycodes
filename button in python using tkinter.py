9a643b5b2ffbe6d4d5128359a429b0f64be1ac6a
Aditi Garg <113931894+purplehacker2022@users.noreply.github.com>
2022-09-29 20:08:58 +0530

    Add files via upload

---
+++ button in python using tkinter.py
@@ -0,0 +1,6 @@
+
+import tkinter as tt
+window= tt.Tk()
+button=tt.Button(text="click here ",fg="black",bg="pink",width=45,height=5)
+button.pack()
+window.mainloop()
---
+++ tkinter in python.py
@@ -0,0 +1,7 @@
+
+#import tkinter module 
+import tkinter as tk
+window = tk.Tk()
+greeting = tk.Label(text="hello,tkinter!\n\tAns.Ans.Ans.Ans.\t\nhello there this is aditi garg.\n nice to meet you\n\n long time no see.",bg="black", fg ="pink",width=50, height=50)
+greeting.pack()
+window.mainloop()
